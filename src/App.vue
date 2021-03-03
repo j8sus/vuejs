f0fae6b64768e8aba007c1d3b0dad18bba1568c5
Sergei Balabanov <sergeibalabanov@gmail.com>
2021-03-03 21:05:51 +0600

    Make TodoList page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo Application</h1>
+    <AddTodo @add-todo="addTodo" />
+    <TodoList v-bind:todos="todos" @remove-todo="removeTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "@/components/TodoList";
+import AddTodo from "@/components/AddTodo";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  mounted() {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => {
+        this.todos = json;
+      });
+  },
   components: {
-    HelloWorld
-  }
-}
+    TodoList,
+    AddTodo,
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter((t) => t.id !== id);
+    },
+    addTodo(todo) {
+      this.todos.push(todo);
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,7 +44,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
+  margin: 0 auto;
+  width: 800px;
   color: #2c3e50;
   margin-top: 60px;
 }
